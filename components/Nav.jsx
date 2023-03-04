e8f3e504710186054a5921e1152585ebbdb5e835
ltphen <tatkeulionel@gmail.com>
2023-03-04 17:59:58 +0100

    feat: First section of the header started

---
+++ components/Nav.jsx
@@ -0,0 +1,56 @@
+import Link from "next/link";
+import Image from "next/image";
+import React, { useCallback, useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import Lottie from "react-lottie";
+import SplitType from "split-type";
+import loader from "../public/loader.json";
+import logoDark from "../public/logo-dark.png";
+
+export const Nav = () => {
+
+  return (
+    <div>
+        <header class="mx-auto max-w-7xl relative z-10 py-4 md:py-6">
+                <div class="container px-4 mx-auto sm:px-6 lg:px-8">
+                    <div class="relative flex items-center justify-between">
+                        <div class="flex-shrink-0">
+                            <a href="#" title="" class="flex rounded outline-none focus:ring-1 focus:ring-gray-900 focus:ring-offset-2">
+                                <img src={"/logo.png"}  className="h-8"/>
+                            </a>
+                        </div>
+                    <div class="flex md:hidden">
+                        <button type="button" class="text-gray-900">
+                            <span class="" aria-hidden="true">
+                                <svg class="w-7 h-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M4 6h16M4 12h16M4 18h16"></path></svg></span><span class="hidden" aria-hidden="true"><svg class="w-7 h-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+                            </span>
+                        </button>
+                    </div>
+                    <div class="hidden md:flex md:items-center md:justify-center md:space-x-10 md:absolute md:inset-y-0 md:left-1/2 md:-translate-x-1/2 lg:space-x-16">
+                        <a href="#" title="" class="text-base text-gray-900 transition-all duration-200 rounded focus:outline-none font-pj hover:text-opacity-50 focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"> Blog </a>
+                        <a href="#" title="" class="text-base text-gray-900 transition-all duration-200 rounded focus:outline-none font-pj hover:text-opacity-50 focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"> Videos </a>
+                        <a href="#" title="" class="text-base text-gray-900 transition-all duration-200 rounded focus:outline-none font-pj hover:text-opacity-50 focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"> About </a>
+                    </div>
+                    <div class="hidden md:flex">
+                        <a href="#" title="" class="inline-flex items-center justify-center px-6 py-3 text-base font-bold leading-7 text-green-400 transition-all duration-200 bg-blue-900 border border-transparent rounded hover:bg-gray-600 font-pj focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 font-extrabold" role="button">Join The Community</a>
+                    </div>
+                </div>
+                <nav class="hidden">
+                    <div class="px-1 py-8">
+                        <div class="grid gap-y-7">
+                            <a href="#" title="" class="flex items-center p-3 -m-3 text-base text-gray-900 transition-all duration-200 rounded hover:bg-gray-50 focus:outline-none font-pj focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"> Blog </a>
+                            <a href="#" title="" class="flex items-center p-3 -m-3 text-base text-gray-900 transition-all duration-200 rounded hover:bg-gray-50 focus:outline-none font-pj focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"> Videos </a>
+                            <a href="#" title="" class="flex items-center p-3 -m-3 text-base text-gray-900 transition-all duration-200 rounded hover:bg-gray-50 focus:outline-none font-pj focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"> About </a>
+                            <a href="#" title="" class="inline-flex items-center justify-center px-6 py-3 text-base font-bold leading-7 text-green-400 transition-all duration-200 bg-blue-900 border border-transparent rounded hover:bg-gray-600 font-pj focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 font-extrabold" role="button">Join The Community</a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+    </header>
+     
+    </div>
+  );
+};
+
+export default Nav;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,165 +1,111 @@
-import Link from "next/link";
-import Image from "next/image";
+
 import React, { useCallback, useEffect, useRef } from "react";
-import { gsap } from "gsap";
-import Lottie from "react-lottie";
-import SplitType from "split-type";
-import loader from "../public/loader.json";
-import logoDark from "../public/logo-dark.png";
+import Nav from "./../components/Nav"
 
 const Index = () => {
-  const animContainerRef = useRef(null);
-  const boxRef = useRef(null);
-  const videoRef = useRef<HTMLVideoElement>(null);
-  const titleRef = useRef<HTMLDivElement>(null);
-  const linesRef = useRef<HTMLDivElement>(null);
-
-  const onLoadingFinish = useCallback(() => {
-    const lines = new SplitType("#lines", { types: "lines" });
-    const titleRef = new SplitType("#talk", { types: "words, chars, chars" });
-
-    gsap
-      .timeline()
-      .to(
-        boxRef.current,
-        {
-          duration: 1,
-          delay: 2,
-          opacity: 0,
-          ease: "BezierEasing(0.19,1,0.22,1)",
-        },
-        "p"
-      )
-      .to(
-        animContainerRef.current,
-        {
-          duration: 1.2,
-          transform: "scale(10, 1)",
-          height: 0,
-        },
-        "-=.5"
-      )
-      .to(animContainerRef.current, {
-        display: "none",
-      })
-      .from(
-        lines.lines,
-        {
-          opacity: 0,
-          y: 50,
-          duration: 0.7,
-          stagger: { amount: 0.5 },
-          ease: "BezierEasing(0.19,1,0.22,1)",
-        },
-        "-=1.5"
-      )
-      .from(
-        titleRef.chars,
-        {
-          opacity: 0,
-          y: 50,
-          duration: 0.7,
-          stagger: { amount: 0.5 },
-          ease: "BezierEasing(0.19,1,0.22,1)",
-        },
-        "-=.5"
-      );
-  }, []);
-
-  useEffect(() => {
-    onLoadingFinish();
-  }, [onLoadingFinish]);
-
-  useEffect(() => {
-    videoRef.current && videoRef.current.play();
-  }, []);
 
   return (
     <div>
-      <header className="h-20 px-20">
-        <div className={"font-medium text-2xl absolute fixed left-4 top-4"}>
-          <Link href="/">
-            <Image width={40} height={40} src={logoDark} alt="logo" />
-          </Link>
-        </div>
-      </header>
-      <main className="px-4 sm:px-14">
-        <div className="flex flex-col justify-between">
-          <div className="flex flex-col lg:flex-row">
-            <div
-              id="lines"
-              ref={linesRef}
-              className="flex flex-col  w-full sm:w-11/12  lg:w-5/12"
-            >
-              <div className="font-bold text-5xl mt-5">
-              Inspiring the tech <br />
-              founders of tomorrow 
+      <Nav></Nav>
+
+      <div className="bg-gray-50">
+           
+
+            <section className="pt-12 pb-12 sm:pb-16 lg:pt-8">
+                <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+                    <div className="grid max-w-lg grid-cols-1 mx-auto lg:max-w-full lg:items-center lg:grid-cols-2 gap-y-12 lg:gap-x-16">
+                        <div>
+                            <div className="text-center lg:text-left">
+                              <img className="w-22" src="/rails.svg" alt="" />
+
+                                <h1 className="text-4xl font-bold leading-tight text-gray-900 sm:text-5xl sm:leading-tight lg:leading-tight lg:text-6xl">Inspiring the tech founders of tomorrow</h1>
+                                <p className="mt-2 text-lg text-gray-600 sm:mt-8 font-inter">Next talk Sunday, 19 February 2023 with the Founder of KapyGPT as Guest</p>
+
+                                <form action="#" method="POST" className="mt-8 sm:mt-10">
+                                    <div className="relative p-2 sm:border bg-gray-200 sm:border-gray-200 group sm:rounded-xl sm:focus-within:ring-1 sm:focus-within:ring-gray-900 sm:focus-within:border-gray-900">
+                                        <input
+                                            type="email"
+                                            name=""
+                                            id=""
+                                            placeholder="Enter email address"
+                                            className="block w-full px-4 py-4 text-gray-900 placeholder-gray-900 bg-transparent border border-gray-400 outline-none focus:border-gray-900 focus:ring-1 focus:ring-gray-900 rounded-xl sm:border-none sm:focus:ring-0 sm:focus:border-transparent"
+                                        />
+                                        <div className="mt-4 sm:mt-0 sm:absolute sm:inset-y-0 sm:right-0 sm:flex sm:items-center sm:pr-2">
+                                            <button type="submit" className="inline-flex px-6 py-3 text-lg font-bold text-green-500 transition-all duration-200 bg-blue-900 rounded-lg focus:outline-none focus:bg-gray-600 hover:bg-gray-600">Get Free Card</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+
+                            <div className="flex items-center justify-center mt-10 space-x-6 lg:justify-start sm:space-x-8">
+                               
+                                <div className="hidden sm:block">
+                                  <img className="w-full" src="/users.png" alt="" />
+
+                                </div>
+
+                                <div className="flex items-center">
+                                    <p className="ml-0 text-sm text-gray-900">50+ active members sharing their tech knowledge</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div>
+                            <img className="w-full" src="/hero.png" alt="" />
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="py-12 bg-white sm:py-16 lg:py-20">
+              <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div className="max-w-5xl mx-auto">
+                  <div className="max-w-md mx-auto text-center">
+                    <img src="/fly.svg" className="w-16 mb-10 mx-auto"/>
+                    <h2 className="text-4xl text-blue-900 font-bold">
+                      The arm of our community
+                    </h2>
+                    <p className="mt-6 text-base  text-gray-700">
+                    We help tech enthusiast and future innovator to be inspired, to share and exchange knowledge
+                    </p>
+                  </div>
+
+                  <div
+                    className="grid max-w-md grid-cols-1 gap-12 mx-auto mt-8 text-center sm:mt-16 md:mx-0 md:max-w-none lg:mt-16 md:text-left lg:mt-20 md:grid-cols-3 xl:gap-x-20">
+                    <div>
+                      <img src="/about1.svg" className=" mx-auto"/>
+                      <h3 className="text-center text-blue-900 mt-6 text-lg font-bold text-gray-900">
+                      Founders Talks
+                      </h3>
+                      <p className="mt-4 text-sm text-center  text-gray-700">
+                      The invited founders that share their knowledge with us
+                      </p>
+                    </div>
+
+                    <div>
+                    <img src="/about2.svg" className=" mx-auto"/>
+                      <h3 className="text-center text-blue-900 mt-6 text-lg font-bold text-gray-900">
+                      Tech exchanges
+                      </h3>
+                      <p className="mt-4 text-sm text-center  text-gray-700">
+                      General topic about tech, startups, products, co-founder matching, ...
+                      </p>
+                    </div>
+
+                    <div>
+                    <img src="/about3.svg" className=" mx-auto"/>
+                      <h3 className="text-center text-blue-900 mt-6 text-lg font-bold text-gray-900">
+                      Debugging sessions
+                      </h3>
+                      <p className="mt-4 text-sm text-center  text-gray-700">
+                      Helping developers overcome their problems by debugging their codes
+                      </p>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className="font-bold text-black-800 w-full lg:w-9/12 mt-10 lg:mt-32">
-              if join == True:<br/>
-              &gt;&gt;&gt;&gt; print(<span className="text-red-500">&quot;Amazing talks every Sunday&quot;</span>)<br/>
-              &gt;&gt;&gt;&gt;  print(<span className="text-blue-500">&quot;Network with other founders&quot;</span>)<br/>
-              &gt;&gt;&gt;&gt;  print(<span className="text-pink-500">&quot;Network with Tech enthusiast&quot;</span>)
-              </div>
-              <ul className="font-bold flex w-9/12 mt-8 mb-12 lg:mb-0 ">
-                <Link href={"https://youtube.com"}>
-                  <li className="h-8 mr-10 flex justify-center items-center border-b-2 hover:border-red-700 hover:text-red-500 cursor-pointer">
-                    Youtube
-                  </li>
-                </Link>
-                <Link href={"https://discord.gg/97HAgvjcCx"}>
-                  <li className="h-8 flex justify-center items-center border-b-2 cursor-pointer hover:border-blue-700 hover:text-blue-500">
-                    Discord
-                  </li>
-                </Link>
-                
-              </ul>
-            </div>
-
-            <div className="flex justify-center items-center w-full lg:w-8/12 overflow-hidden relative h-52 sm:h-72 md:h-96 lg:h-128">
-              <video
-                ref={videoRef}
-                className="absolute -top-28 pt-14 md:pt-0"
-                src="/TechTalks_Speed.mp4"
-                muted
-                autoPlay
-                loop
-              />
-            </div>
-          </div>
-
-          <div className="font-extrabold flex items-center h-60 pt-5 text-8xl md:text-9xl lg:text-10xl xl:text-11xl">
-            <span id="talk" ref={titleRef}>
-              TECH TALKS
-            </span>
-          </div>
+            </section>
         </div>
 
-        <div
-          ref={animContainerRef}
-          className="absolute bottom-0 left-0 bg-blue-600 w-screen h-screen flex flex-col items-center justify-center z-50 overflow-hidden"
-        >
-          <div
-            className="flex flex-col justify-center items-center"
-            ref={boxRef}
-          >
-            <div className="relative top-20">
-              <Link href="/">
-                <Image width={40} height={40} src={logoDark} alt="logo" />
-              </Link>
-            </div>
-
-            <Lottie
-              options={{
-                loop: true,
-                autoplay: true,
-                animationData: loader,
-              }}
-              height={250}
-            />
-          </div>
-        </div>
-      </main>
     </div>
   );
 };
